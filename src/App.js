e39168b795258f4c54affc9648b120e0aa5d3832
Sergei Abramov <abrams.web@gmail.com>
2022-11-09 17:29:34 +0300

    shit commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import './styles/App.css';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
-  
+        const [posts, setPosts] = useState ([
+          {id:1, title: 'Javascript', body:'Description'},
+          {id:2, title: 'Javascript', body:'Description'},
+          {id:3, title: 'Javascript', body:'Description'},
+        ])
 
   return (
   <div className = 'App'>
-    <Counter/>
-    <ClassCounter/>
-    <PostItem post={{id:1, title: 'Javascript', body:'Description'}}/>
+    <form>
+      <input type="text" placeholder="Назвавание поста"/>
+      <input type="text" placeholder="Описание поста"/>
+      <MyButton>Создать пост</MyButton>
+    </form>
+    <PostList posts={posts} title ="Список постов 1"/>
   </div>
   );
 }
